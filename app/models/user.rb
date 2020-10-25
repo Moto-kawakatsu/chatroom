5b6508ac19d7ba78e752b2b7fb9da7fdda202f2f
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-10-25 20:32:11 +0900

    ユーザー登録情報の入力欄

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,33 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   extend ActiveHash::Associations::ActiveRecordExtensions
+
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable,  :validatable
+  
   belongs_to_active_hash :genre
   belongs_to_active_hash :grade
   belongs_to_active_hash :class_number
   belongs_to_active_hash :number
-  
-
-
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable,  :validatable
-  validates :name, :email, :password, :genre, :grade, :class_number, :number, presence: true
-  validates :genre_id, :grade_id, :class_id, :class_number_id, :number_id,  numericality: { other_than: 1 }
 
   has_many :room_users
   has_many :rooms, through: :room_users
   has_many :messages
+  
+
+
+ 
+        
+ with_options presence: true do
+   validates :name
+   validates :email
+   validates :password
+
+  with_options numericality: { other_than: 1} do
+    validates :genre_id
+    validates :grade_id
+    validates :class_number_id
+    validates :number_id
+  end
+ end
 end
